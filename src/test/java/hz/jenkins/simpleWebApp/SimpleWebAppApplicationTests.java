5c863e159c3415fffe8b25ea2e45bd33de938774
haithem <haithem.zouari3@gmail.com>
2022-12-25 12:29:05 +0100

    create project

---
+++ src/test/java/hz/jenkins/simpleWebApp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.jenkins.simpleWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
